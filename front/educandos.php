52456dfac1cfe9c5f2f0be42cb11728f53fddc5a
Rodrigo Menéndez Molina <menendez.rodrigo555@gmail.com>
2026-02-09 17:02:24 +0100

    a

--- front/educandos.php
+++ front/educandos.php
@@ -1,30 +1,72 @@
-<?php
+<?php 
 include("inc/header.html")
 ?>
 
 <style>
-    main{
-        display: flex;
-        flex-direction: row;
-    }
+main{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    background: aliceblue;
+}
+
+/* ===== COLUMNA PERFIL ===== */
+section.izquierda{
+    background: #b3b3b3;
+    width: 350px;
+    padding: 30px;
+    text-align: center;
+}
+
+section.izquierda p{
+    margin: 20px 0;
+    font-size: 18px;
+}
+
+section.izquierda {
+    margin-bottom: 30px;
+}
+/* ===== COLUMNA HIJOS ===== */
+section.derecha{
+    background: #b3b3b3;
+    width: 350px;
+    padding: 30px;
+    text-align: center;
+}
+
+section.derecha {
+    margin-bottom: 30px;
+}
+
+/* ===== BOTONES HIJOS ===== */
+.documentacion{
+    padding: 15px;
+    margin: 20px 0;
+    font-size: 18px;
+    cursor: pointer;
+    background: white;
+}
+
 </style>
 
 <main>
     <section class="izquierda">
-        <h1>Nombre y apellidos</h1>
-        <p>Seccion</p>
+        <h1>Hijo1</h1>
+        <p>Sección</p>
         <p>Año</p>
-        <p>Domicilio</p>
+        <p>DNI</p>
     </section>
-    <section class="derecha">
-        <h1>Hijos</h1>
-        <?php
 
-        ?>
+    <section class="derecha">
+        <h1>Documentación</h1>
+
+        <div class="documentacion">algo</div>
+        <div class="documentacion">algo</div>
+        <div class="documentacion">algo</div>
+
     </section>
 </main>
 
-
-<?php
+<?php 
 include("inc/footer.html")
 ?>
--- front/perfil.php
+++ front/perfil.php
@@ -1,12 +1,62 @@
-<?php
+<?php 
 include("inc/header.html")
 ?>
 
 <style>
-    main{
-        display: flex;
-        flex-direction: row;
-    }
+main{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    background: aliceblue;
+}
+
+/* ===== COLUMNA PERFIL ===== */
+section.izquierda{
+    background: #b3b3b3;
+    width: 350px;
+    padding: 30px;
+    text-align: center;
+}
+
+section.izquierda p{
+    margin: 20px 0;
+    font-size: 18px;
+}
+
+section.izquierda {
+    margin-bottom: 30px;
+}
+/* ===== COLUMNA HIJOS ===== */
+section.derecha{
+    background: #b3b3b3;
+    width: 350px;
+    padding: 30px;
+    text-align: center;
+}
+
+section.derecha {
+    margin-bottom: 30px;
+}
+
+/* ===== BOTONES HIJOS ===== */
+.hijo{
+    padding: 15px;
+    margin: 20px 0;
+    font-size: 18px;
+    cursor: pointer;
+}
+
+.hijo1{
+    background: #ffe066;
+}
+
+.hijo2{
+    background: #4c6ef5;
+}
+
+.hijo3{
+    background: #00c46a;
+}
 </style>
 
 <main>
@@ -17,15 +67,17 @@ include("inc/header.html")
         <p>Email</p>
         <p>Domicilio</p>
     </section>
+
     <section class="derecha">
         <h1>Hijos</h1>
-        <?php
 
-        ?>
+        <div class="hijo hijo1">hijo1</div>
+        <div class="hijo hijo2">hijo2</div>
+        <div class="hijo hijo3">hijo3</div>
+
     </section>
 </main>
 
-
-<?php
+<?php 
 include("inc/footer.html")
 ?>
